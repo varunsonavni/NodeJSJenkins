ea5e60a66338775c9da6c4a2e012227355d3d7ae
varunsonavni <102613598+varunsonavni@users.noreply.github.com>
2022-04-29 11:16:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'varun the .Beast Incarnate.'
-  res.end(msg);
-});
+app.get('/', (req, res) => {
+  res.send('<h1>My new  Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.5</p>');
+ })
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
 
-});
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
