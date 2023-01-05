fb9eb1c1ac0cd57e0ba8bef8deb5acfe176e3cc3
varunsonavni <102613598+varunsonavni@users.noreply.github.com>
2023-01-05 11:01:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>In Stage App</h1> <h4>Message: Success Done Updating</h4> <p>Version 1.9.0</p>');
+  res.send('<h1>In PROD $$$ App</h1> <h4>Message: Success Done Updating</h4> <p>Version 1.9.0</p>');
  })
 
 
